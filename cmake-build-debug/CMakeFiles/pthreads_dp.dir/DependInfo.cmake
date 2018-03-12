ed6e14980d3e82797695cee51792aa3996ff2ca8
Zacharygil <Zaculagil@gmail.com>
2018-03-11 22:42:27 -0500

    Add existing file

---
+++ cmake-build-debug/CMakeFiles/pthreads_dp.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/Zacula/Desktop/pthreads_dp/dining_philosophers.c" "/Users/Zacula/Desktop/pthreads_dp/cmake-build-debug/CMakeFiles/pthreads_dp.dir/dining_philosophers.c.o"
+  "/Users/Zacula/Desktop/pthreads_dp/dp_asymmetric.c" "/Users/Zacula/Desktop/pthreads_dp/cmake-build-debug/CMakeFiles/pthreads_dp.dir/dp_asymmetric.c.o"
+  "/Users/Zacula/Desktop/pthreads_dp/dp_waiter.c" "/Users/Zacula/Desktop/pthreads_dp/cmake-build-debug/CMakeFiles/pthreads_dp.dir/dp_waiter.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/pthreads_dp.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pthreads_dp.dir/dining_philosophers.c.o"
+  "CMakeFiles/pthreads_dp.dir/dp_asymmetric.c.o"
+  "CMakeFiles/pthreads_dp.dir/dp_waiter.c.o"
+  "pthreads_dp.pdb"
+  "pthreads_dp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/pthreads_dp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
